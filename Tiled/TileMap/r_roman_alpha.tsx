ae13b1c9b835b5cbf2ea2ba4a1c2153da042cf06
AlecFaye <kelvintran.lim@outlook.com>
2020-11-11 10:53:36 -0500

    Removed tile files

--- Tiled/TileMap/r_roman_alpha.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="r_roman_alpha" tilewidth="256" tileheight="128" tilecount="56" columns="7">
- <image source="r_roman_alpha.png" width="1920" height="1080"/>
-</tileset>
